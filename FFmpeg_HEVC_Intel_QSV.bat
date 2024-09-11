0a2f3f73802ceff52f344a0c026f52afbfa3fbec
ooopus <107778929+ooopus@users.noreply.github.com>
2024-09-11 16:30:27 +0800

    Update FFmpeg_HEVC_Intel_QSV.bat

--- FFmpeg_HEVC_Intel_QSV.bat
+++ FFmpeg_HEVC_Intel_QSV.bat
@@ -1,65 +1,68 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM 获取输入参数
+REM Get the input parameters
 set "input_file=%~1"
 
-REM 提取输入文件所在文件夹路径
+REM Extract the input file's folder path
 for %%F in ("%input_file%") do (
-    set "input_folder=%%~dpF"
-    set "input_filename=%%~nF"
+   set "input_folder=%%~dpF"
+   set "input_filename=%%~nF"
 )
 
-REM 生成输出文件路径
+set block_number=0
+
+REM Generate the output file path
 set "original_file=%input_folder%%input_filename%.mp4"
 set "output_file=%input_folder%%input_filename%_optimized.mp4"
 set "output_filename=%input_folder%%input_filename%_optimized"
 
-REM 获取原始文件大小
-for /f %%i in ('ffprobe -v error -show_entries format^=size -of default^=nw^=1:nk^=1 "%original_file%"') do set "original_file_size_byte=%%i"
-
-REM 调用 PowerShell 进行大数加法运算并向上取整
-for /f %%A in ('powershell -command "[math]::Ceiling([decimal](%original_file_size_byte%) / [decimal](2000000000))"') do set "block_number=%%A"
-
-REM 获取原始文件时长
-for /f %%a in ('ffprobe -v error -show_entries format^=duration -of default^=noprint_wrappers^=1:nokey^=1 "%original_file%"') do set "original_file_duration_second=%%a"
-
-REM 计算分段时长
-for /f %%A in ('powershell -command "[math]::Ceiling([decimal](%original_file_duration_second%) / [decimal](%block_number%))"') do set "segment_duration=%%A"
-
-REM 检测是否存在已优化的文件
+REM Check if the optimized file already exists
 if exist "%output_file%" (
-    echo 已优化文件存在，跳过第一部分处理。
-    goto skip_first_part
+   echo The optimized file already exists, skipping the first part of the process.
+   goto skip_first_part
 )
 
-
-
-echo original_file_path: %original_file%
-echo original_file_size_byte: %original_file_size_byte%
-echo block_number: %block_number%
-echo original_file_duration_second: %original_file_duration_second%
-echo segment_duration: %segment_duration%
-
-REM 第一部分：编码为 HEVC 格式
+REM First part: Encode to HEVC format
 ffmpeg -init_hw_device qsv=hw -filter_hw_device hw -i "%input_file%" -c:v hevc_qsv -preset slow -global_quality 22 -movflags +faststart "%output_file%"
 if errorlevel 1 (
-    echo 第一部分处理失败
-    goto :eof
+   echo First part processing failed
+   goto :eof
 )
 
 :skip_first_part
-echo original_file_path: %original_file%
-echo original_file_size_byte: %original_file_size_byte%
+
+REM Get the original file size
+for /f %%i in ('ffprobe -v error -show_entries format^=size -of default^=nw^=1:nk^=1 "%output_file%"') do set "output_file_size_byte=%%i"
+
+REM Call PowerShell for big number addition and take the ceiling
+for /f %%A in ('powershell -command "[math]::Ceiling([decimal](%output_file_size_byte%) / [decimal](2000000000))"') do set "block_number=%%A"
+
+REM Get the original file duration
+for /f %%a in ('ffprobe -v error -show_entries format^=duration -of default^=noprint_wrappers^=1:nokey^=1 "%output_file%"') do set "output_file_duration_second=%%a"
+
+REM Calculate the segment duration
+for /f %%A in ('powershell -command "[math]::Ceiling([decimal](%output_file_duration_second%) / [decimal](%block_number%))"') do set "segment_duration=%%A"
+
+echo output_file_path: %output_file%
+echo output_file_size_byte: %output_file_size_byte%
 echo block_number: %block_number%
-echo original_file_duration_second: %original_file_duration_second%
+echo output_file_duration_second: %output_file_duration_second%
 echo segment_duration: %segment_duration%
-REM 第二部分：分割文件
-ffmpeg -i "%output_file%" -c copy -map 0 -segment_time %segment_duration% -f segment -reset_timestamps 1 "%output_filename%_%%03d.mp4"
-if errorlevel 1 (
-    echo 第二部分处理失败
-    goto :eof
+
+if %block_number% gtr 1 (
+   echo block_number: %block_number% is greater than 1, continue executing the script
+) else (
+   echo block_number: %block_number% is less than or equal to 1, the script exits
+   exit /b 1
 )
 
-echo 处理完成
+REM Second part: Split the file
+ffmpeg -i "%output_file%" -c copy -map 0 -segment_time %segment_duration% -f segment -reset_timestamps 1 "%output_filename%_%%03d.mp4"
+if errorlevel 1 (
+   echo Second part processing failed
+   goto :eof
+)
+
+echo Processing completed
 pause
