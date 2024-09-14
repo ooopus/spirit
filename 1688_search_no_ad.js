eb9a9b9f91c78a29e31ac12ea6719be07348af03
ooopus <107778929+ooopus@users.noreply.github.com>
2024-09-14 10:03:02 +0800

    Update 1688_search_no_ad.js

--- 1688_search_no_ad.js
+++ 1688_search_no_ad.js
@@ -1,23 +1,26 @@
 (function() {
-    let hostname = window.location.hostname;
+    const hostname = window.location.hostname;
     if (hostname === 's.1688.com') {
         process();
-        let observer = new MutationObserver(process);
+        const observer = new MutationObserver(process);
         observer.observe(document.body, { childList: true, subtree: true });
     }
 
     function process() {
         // 查找所有 main-img-icon 的 img 元素
-        let mainImgIcons = document.querySelectorAll('img.main-img-icon');
-        if (mainImgIcons && mainImgIcons.length > 0) {
+        const mainImgIcons = document.querySelectorAll('img.main-img-icon');
+        if (mainImgIcons.length > 0) {
+            const offersToRemove = [];
             mainImgIcons.forEach(img => {
                 // 获取其对应的 a.search-offer-wrapper.cardui-normal.search-offer-item.major-offer 父级元素
-                let offerWrapper = img.closest('a.search-offer-wrapper.cardui-normal.search-offer-item.major-offer');
+                const offerWrapper = img.closest('a.search-offer-wrapper.cardui-normal.search-offer-item.major-offer');
                 if (offerWrapper && offerWrapper.parentNode) {
-                    // 移除对应的广告父元素
-                    offerWrapper.parentNode.removeChild(offerWrapper);
+                    // 收集需要移除的广告父元素
+                    offersToRemove.push(offerWrapper);
                 }
             });
+            // 一次性移除所有收集到的广告父元素
+            offersToRemove.forEach(offer => offer.parentNode.removeChild(offer));
         }
     }
 })();
