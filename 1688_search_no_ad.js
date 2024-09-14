9a69f13d907140c0d71f841cb07bb91452bb7ef7
ooopus <107778929+ooopus@users.noreply.github.com>
2024-09-14 10:00:25 +0800

    Update 1688_search_no_ad.js

--- 1688_search_no_ad.js
+++ 1688_search_no_ad.js
@@ -1,31 +1,23 @@
 (function() {
     let hostname = window.location.hostname;
-    if (hostname == 's.1688.com') {
+    if (hostname === 's.1688.com') {
         process();
-        // 使用 MutationObserver 替代 DOMSubtreeModified 以提高性能
         let observer = new MutationObserver(process);
         observer.observe(document.body, { childList: true, subtree: true });
     }
 
     function process() {
-        // 移除 main-img-icon 广告
-        let mainImgIcons = document.getElementsByClassName('main-img-icon');
-        removeElements(mainImgIcons);
-        
-        // 移除 ad-item 广告
-        let adItems = document.getElementsByClassName('ad-item');
-        removeElements(adItems);
-    }
-
-    function removeElements(elements) {
-        // 遍历元素并移除其父级节点
-        if (elements && elements.length > 0) {
-            for (let i = elements.length - 1; i >= 0; i--) {
-                let parent = elements[i].parentNode;
-                if (parent && parent.parentNode) {
-                    parent.parentNode.removeChild(parent);
+        // 查找所有 main-img-icon 的 img 元素
+        let mainImgIcons = document.querySelectorAll('img.main-img-icon');
+        if (mainImgIcons && mainImgIcons.length > 0) {
+            mainImgIcons.forEach(img => {
+                // 获取其对应的 a.search-offer-wrapper.cardui-normal.search-offer-item.major-offer 父级元素
+                let offerWrapper = img.closest('a.search-offer-wrapper.cardui-normal.search-offer-item.major-offer');
+                if (offerWrapper && offerWrapper.parentNode) {
+                    // 移除对应的广告父元素
+                    offerWrapper.parentNode.removeChild(offerWrapper);
                 }
-            }
+            });
         }
     }
 })();
